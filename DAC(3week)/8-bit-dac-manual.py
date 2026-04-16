96f30e8548a9a363b653c1e8bbf01a40508ecef8
blessedBA <lana.pchelket@google.com>
2026-04-16 18:50:35 +0300

    manual DACgit status!

---
+++ DAC(3week)/8-bit-dac-manual.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
+
+GPIO.setup(dac_bits, GPIO.OUT)
+
+dynamic_range = 3.2643453452
+
+def voltage_to_number(voltage):
+    if not (0.0 <= voltage <= dynamic_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
+        print("Устанавливаем 0.0 В")
+        return 0
+
+    return int(voltage / dynamic_range * 255)
+
+def number_to_dac(number):
+    GPIO.output(dac_bits, [int(element) for element in bin(number)[2:].zfill(8)])
+    return 0
+
+try:
+    while True:
+        try:
+            voltage = float(input("Enter voltage in Volts: "))
+            number = voltage_to_number(voltage)
+            number_to_dac(number)
+
+        except ValueError:
+            print("you entered not a number. Try again\n")
+
+finally:
+    GPIO.output(dac_bits, 0)
+    GPIO.cleanup()
