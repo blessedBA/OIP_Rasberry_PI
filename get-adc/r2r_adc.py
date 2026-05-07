20c14e3cd8584fa4ef45e2aa2a01dbdadb0b091b
blessedBA <lana.pchelket@google.com>
2026-05-07 18:16:32 +0300

    8bit adc work good

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -1,8 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import dec2bin
-
-
 
 class R2R_ADC:
 
@@ -17,15 +14,35 @@ class R2R_ADC:
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
-        GPIO.setup(self.comp_gpio. GPIO.IN)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
 
     def deinit(self):
         GPIO.output(self.bits_gpio, 0)
-        GPIO.output(self.comp_gpio, 0)
         GPIO.cleanup()
         
     def number_to_dac(self, number):
-        GPIO.output(self.bits_gpio, dec2bin(number))
+        bits = [int(element) for element in bin(number)[2:].zfill(8)]
+        GPIO.output(self.bits_gpio, bits)
 
-    def sequentil_counting_adc(self):
-        
+    def sequential_counting_adc(self):
+        for i in range(0, 255):
+            self.number_to_dac(i)
+            time.sleep(0.01)
+            if (GPIO.input(self.comp_gpio) == 1):
+                return i
+
+        return 255
+
+    def get_sc_voltage(self):
+        value = self.sequential_counting_adc()
+        return (value / 255) * self.dynamic_range
+
+if __name__ == "__main__":
+    try:
+        adc = R2R_ADC(3.3, 0.01)
+        while True:
+            voltage = adc.get_sc_voltage()
+            print (f"Напряжение: {voltage}")
+
+    finally:
+        adc.deinit()
